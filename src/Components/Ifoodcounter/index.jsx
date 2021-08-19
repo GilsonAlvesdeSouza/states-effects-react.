652290b04e6f16a389210275f00cd465c3486e3e
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2021-08-19 14:37:26 -0300

    Finalizado o componente ifoos

--- src/Components/Ifoodcounter/index.jsx
+++ src/Components/Ifoodcounter/index.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import { Wrapper } from './styled';
+import React, { useState } from 'react'
+import { ButtonMinusActive, ButtonMinusDesactive, ButtonPlusActive, Wrapper } from './styled';
 
 export const IfoodCounter = () => {
+    const [value, setValue] = useState(0);
+
+    const incremento = () => {
+        setValue(value + 1);
+    }
+
+    const decremento = () => {
+        if (value > 0) setValue(value - 1);
+    }
+
+    const AtivarDesativarMinus = (value) => {
+        if (value > 0) {
+            return <ButtonMinusActive onClick={decremento}> - </ButtonMinusActive>;
+        }
+        return <ButtonMinusDesactive> - </ButtonMinusDesactive>;
+    };
+
     return (
         <Wrapper>
-            <h1>Funcionou</h1>
+            <ButtonPlusActive onClick={incremento}>+</ButtonPlusActive>
+            <p>{value}</p>
+            {/* 2 formas de se fazer */}
+            {/* {value < 1 ? <ButtonMinusDesactive>-</ButtonMinusDesactive> : <ButtonMinusActive onClick={decremento}>-</ButtonMinusActive>} */}
+            {AtivarDesativarMinus(value)}
         </Wrapper>
     )
 }
